3d4a147d8356f7f2dfb3dccdf33fcc2a93808d15
Carlos <ccaieg00@estudiantes.unileon.es>
2024-04-06 19:43:43 +0200

    add content structure

--- app/src/main/java/com/example/myinstagramapp/LoginScreen.kt
+++ app/src/main/java/com/example/myinstagramapp/LoginScreen.kt
@@ -1,25 +1,48 @@
 package com.example.myinstagramapp
 
 import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen() {
-    Box(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
         Header(Modifier.align(Alignment.TopEnd))
-        Content()
+        Content(Modifier.align(Alignment.Center))
         Footer()
     }
 }
@@ -41,8 +64,109 @@ fun Header(modifier: Modifier) {
 }
 
 @Composable
-fun Content() {
+fun Content(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            painter = painterResource(id = R.drawable.insta),
+            contentDescription = "Instagram"
+        )
 
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp, start = 8.dp, end = 8.dp),
+            value = "",
+            label = { Text(text = "Phone number, username or email") },
+            onValueChange = {},
+            singleLine = true
+        )
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, start = 8.dp, end = 8.dp),
+            value = "",
+            label = { Text(text = "Password") },
+            onValueChange = {},
+            singleLine = true
+        )
+
+
+        Text(
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(top = 12.dp, end = 8.dp)
+                .clickable { },
+            text = "Forgot password?",
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            color = colorResource(id = R.color.bright_blue)
+        )
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp, start = 8.dp, end = 8.dp)
+                .align(Alignment.CenterHorizontally),
+            onClick = {},
+            shape = RoundedCornerShape(4.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = colorResource(id = R.color.bright_blue),
+                disabledContentColor = Color.White,
+                disabledContainerColor = colorResource(id = R.color.baby_blue)
+            )
+        ) {
+            Text(text = "Log In")
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, start = 8.dp, end = 8.dp)
+                .height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Divider(
+                modifier = Modifier
+                    .weight(1f),
+                color = Color.LightGray
+            )
+            Text(
+                text = "OR",
+                color = Color.LightGray,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .padding(horizontal = 24.dp)
+                    .wrapContentWidth(align = Alignment.CenterHorizontally)
+            )
+            Divider(
+                modifier = Modifier
+                    .weight(1f),
+                color = Color.LightGray
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp, start = 8.dp, end = 8.dp)
+                .height(20.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(painter = painterResource(id = R.drawable.fb), contentDescription = "Facebook")
+            Text(
+                modifier = Modifier.padding(start = 4.dp),
+                text = "Continue as Dave Johnson",
+                color = colorResource(id = R.color.bright_blue),
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+
+    }
 }
 
 @Composable
